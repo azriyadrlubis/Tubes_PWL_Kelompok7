d49d2a6e5cea4da116ad0832ba54c0cb9e240e42
vshndrr-code <vshndrr@gmail.com>
2026-05-18 11:20:53 +0700

    nambahin onboarding page

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/onboarding', function () {
+    return view('onboarding');
+})->middleware(['auth', 'verified'])->name('onboarding');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
